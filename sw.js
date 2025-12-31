6b31583a391d96badd8ff719f2d458e276f4ec4f
mattjowen1991-hue <mattjowen1991@gmail.com>
2025-12-31 23:59:43 +0000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'nfl-league-v1';
+const urlsToCache = [
+  '/nfl-regular-season/',
+  '/nfl-regular-season/index.html'
+];
+
+// Install service worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch from cache, fall back to network
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
